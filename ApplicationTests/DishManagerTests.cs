063a13d6091935d45491169e6d25ecdb7a66c40f
Fábio Andrey Salles <fabiosallesctba@gmail.com>
2022-11-21 18:48:24 +0000

    Preserve existing requirements covered by tests

--- ApplicationTests/DishManagerTests.cs
+++ ApplicationTests/DishManagerTests.cs
@@ -41,5 +41,25 @@ namespace ApplicationTests
             Assert.AreEqual("steak", actual.First().DishName);
             Assert.AreEqual(1, actual.First().Count);
         }
+
+        [TestCase(1,"steak")]
+        [TestCase(2,"potato")]
+        [TestCase(3,"wine")]
+        [TestCase(4,"cake")]
+        public void ListWithOneReturnsOneDish(int input, string expected)
+        {
+            var order = new Order
+            {
+                Dishes = new List<int>
+                {
+                    input
+                }
+            };
+
+            var actual = _sut.GetDishes(order);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(expected, actual.First().DishName);
+            Assert.AreEqual(1, actual.First().Count);
+        }
     }
 }
